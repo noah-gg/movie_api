ff742a31ef38548af803b7586c6ca94313d13f36
noah-gg <99127702+noah-gg@users.noreply.github.com>
2022-05-27 10:51:47 +0100

    Mongoose with MongoDB, and Documentation
    If applied, this commit will:
    - install and implement Mongoose, the business logic, to create schema and models, and to export models. Using the MongoDB database from previous task.
    - implement querying and logic (index.js)
    - using Postman to test with sample data

--- index.js
+++ index.js
@@ -1,114 +1,23 @@
 const express = require('express'),
 	morgan = require('morgan'),
 	bodyParser = require('body-parser'),
-	uuid = require('uuid');
+	uuid = require('uuid'),
+	mongoose = require('mongoose'), // Import mongoose
+	Models = require('./models.js'); // Import models.js
+const res = require('express/lib/response');
+
+const Movies = Models.Movie; // Refer to movie model defined in models.js
+const Users = Models.User; // Refer to user model defined in models.js
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 const app = express();
-
-let users = [
-	{
-		id: 1,
-		userName: 'johnny30',
-		fullName: 'John Doe',
-		emailAddress: 'johndoeABC@gmail.com',
-		favourites: ['Your Name', 'Ponyo'],
-	},
-
-	{
-		id: 2,
-		userName: 'suzee30',
-		fullName: 'Suzy May',
-		emailAddress: 'suzymayABC@gmail.com',
-		favourites: ['Bubble', 'NiNoKuni'],
-	},
-];
-
-let movies = [
-	{
-		title: 'Your Name',
-		description:
-			"A bored girl in the countryside starts sporadically waking up in the body of a city boy who's living the exciting life she'd always dreamed of.",
-		genre: 'Romance Anime',
-		director: { name: 'Makoto Shinkai', bio: 'Makoto Shinkai bio' },
-	},
-	{
-		title: 'Bubble',
-		description:
-			'In an abandoned Tokyo overrun by bubbles and gravitational abnormalities, one gifted young man has a fateful meeting with a mysterious girl.',
-		genre: 'Sci-fi Films',
-		director: { name: 'Tetsuro Araki', bio: 'director bio' },
-	},
-	{
-		title: 'Ponyo',
-		description:
-			'A 5-year-old boy named Sosuke forges a friendship with a goldfish princess named Ponyo, who desperately wants to become human.',
-		genre: 'Family Features',
-		director: { name: 'Hayao Miyazaki', bio: 'director bio' },
-	},
-	{
-		title: 'A Whisker Away',
-		description:
-			"A peculiar girl transforms into a cat to catch her crush's attention. But before she realizes it, the line between human and animal starts to blur.",
-		genre: 'Fantasy',
-		director: [
-			{ name: 'Junichi Sato', bio: 'director bio' },
-			{ name: 'Tomotaka Shibayama', bio: 'director bio' },
-		],
-	},
-	{
-		title: 'Flavours of Youth',
-		description:
-			'Memories in a bowl of steaming noodles, a fading beauty finding her way and a bittersweet first love -- all in these stories of city life in China.',
-		genre: 'Japanese',
-		director: { name: 'Xiaoxin Yi', bio: 'director bio' },
-	},
-	{
-		title: 'The Daily Life of the Immortal King',
-		description:
-			'As a cultivation genius who has achieved a new realm every two years since he was a year old, Wang Ling is a near-invincible existence with prowess far beyond his control.',
-		genre: 'School Anime',
-		director: { name: 'Ku Xuan', bio: 'director bio' },
-	},
-	{
-		title: 'Tokyo Godfathers',
-		description:
-			"After finding an abandoned baby on Christmas Eve, three homeless people go in search of the child's parents, meeting a host of oddballs along the way.",
-		genre: 'LGBTQ Films',
-		director: { name: 'Satoshi Kon', bio: 'director bio' },
-	},
-	{
-		title: 'New Gods: Nezha Reborn',
-		description:
-			'While living as an ordinary deliveryman and motor racing fan, Nezha encounters old nemeses and must rediscover his powers to protect his loved ones.',
-		genre: 'Anime Action',
-		director: { name: 'Zhao Ji', bio: 'director bio' },
-	},
-	{
-		title: 'GANTZ:O',
-		description:
-			"Teams of recently deceased people who've been revived and given high-tech weapons must cooperate to defeat an army of monsters in Tokyo and Osaka.",
-		genre: 'Anime Action',
-		director: [
-			{ name: 'Keiichi Sato', bio: 'director bio' },
-			{ name: 'Yashushi Kawamura', bio: 'director bio' },
-		],
-	},
-	{
-		title: 'NiNoKuni',
-		description:
-			'Two average teens go on a magical quest to save the life of their friend and her counterpart from another world. But love complicates their journey.',
-		genre: 'Japanese',
-		director: { name: 'Yoshiyuki Momose', bio: 'director bio' },
-	},
-];
-
-//	Morgan middleware - logs request
+app.use(bodyParser.json()); //	Body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
-
-//	Body-parser
-app.use(bodyParser.json());
-
-//	Serving Static Files - Documentation.html file
 app.use(express.static('public'));
 
 //	GET requests
@@ -116,95 +25,171 @@ app.get('/', (request, response) => {
 	response.send('Welcome to the Animate');
 });
 
-app.get('/movies', (request, response) => {
-	response.json(movies);
+//	Returns a list of all movies
+app.get('/movies', (req, res) => {
+	Movies.find()
+		.then((movies) => {
+			res.status(200).json(movies);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
 });
 
-app.get('/documentation', (request, response) => {
+//	Get documentation
+app.get('/documentation', (req, res) => {
 	response.sendFile('/public/documentation.html', { root: __dirname });
 });
 
-//	Returns data on selected movie
-app.get('/movies/:title', (req, res) => {
-	res.json(
-		movies.find((movie) => {
-			return movie.title === req.params.title;
+//	Returns data about a single movie by title to the user
+app.get('/movies/:Title', (req, res) => {
+	Movies.findOne({ Title: req.params.Title })
+		.then((movie) => {
+			res.json(movie);
 		})
-	);
-	res.send('Request was successful');
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
 });
 
-//	Returns data on selected genre
-app.get('/movies/genre/:genre', (req, res) => {
-	res.json(
-		movies.find((movie) => {
-			return movie.genre === req.params.genre;
+//	Returns data about a genre
+app.get('/movies/Genre/:Name', (req, res) => {
+	Movies.findOne({ 'Genre.Name': req.params.Name })
+		.then((movie) => {
+			res.status(201).json(movie.Genre);
 		})
-	);
-	if (result) {
-		res.status(200).send('Request was successful');
-	} else {
-		res.status(400).send('Cannot find any for genre');
-	}
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error ' + err);
+		});
 });
 
 //	Returns data on selected director
-app.get('/movies/director/:name', (req, res) => {
-	res.json(
-		movies.find((movie) => {
-			return movie.director.name === req.params.name;
+app.get('/movies/Director/:Name', (req, res) => {
+	Movies.findOne({ 'Director.Name': req.params.Name })
+		.then((movie) => {
+			res.status(201).json(movie.Director);
 		})
-	);
-	if (result) {
-		res.status(200).send('Request was successful');
-	} else {
-		res.status(400).send('Cannot find any for genre');
-	}
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
+});
+
+// Get all users
+app.get('/users', (req, res) => {
+	Users.find()
+		.then((users) => {
+			res.status(201).json(users);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
 });
 
 //	Adds new user data to the list of users
 app.post('/users', (req, res) => {
-	let newUser = req.body;
-
-	if (!newUser.name) {
-		const message = 'Please enter new username';
-		res.status(400).send(message);
-	} else {
-		newUser.id = uuid.v4();
-		users.push(newUser);
-		res.status(201).send(newUser);
-	}
+	Users.findOne({ Username: req.body.Username })
+		.then((user) => {
+			if (user) {
+				return res.status(400).send(req.body.Username + ' already exists');
+			} else {
+				Users.create({
+					Username: req.body.Username,
+					Password: req.body.Password,
+					Email: req.body.Email,
+					Birthday: req.body.Birthday,
+				})
+					.then((user) => {
+						res.status(201).json(user);
+					})
+					.catch((error) => {
+						console.error(error);
+						res.status(500).send('Error: ' + error);
+					});
+			}
+		})
+		.catch((error) => {
+			console.error(error);
+			res.status(500).send('Error: ' + error);
+		});
 });
 
-//	Allows users to update their username
-app.put('/users/:userName', (req, res) => {
-	res.send('Request was successful - user data updated');
+//	Allows users to update their info
+app.put('/users/:Username', (req, res) => {
+	Users.findOneAndUpdate(
+		{ Username: req.params.Username },
+		{
+			$set: {
+				Username: req.body.Username,
+				Password: req.body.Password,
+				Email: req.body.Email,
+				Birthday: req.body.Birthday,
+			},
+		},
+		{ new: true }, // this line makes sure that the updated docuement is returned
+		(err, updatedUser) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send('Error: ' + err);
+			} else {
+				res.json(updatedUser);
+			}
+		}
+	);
 });
 
-// 	Deletes user from the list of users using ID
-app.delete('/users/:id', (req, res) => {
-	//	Code wasnt working - need to review again
-	// let user = users.find((user) => {
-	// 	return user.id === req.params.id;
-	// });
-
-	// if (user) {
-	// 	users = user.filter((user) => {
-	// 		return obj.id !== req.params.id;
-	// 	});
-	// 	res.status(201).send('User ' + req.params.id + ' was deleted.');
-	// }
-	res.send('User successfully removed');
+// 	Deletes user from the list of users
+app.delete('/users/:Username', (req, res) => {
+	Users.findOneAndRemove({ Username: req.params.Username })
+		.then((user) => {
+			if (!user) {
+				res.status(400).send(req.params.Username + ' was not found');
+			} else {
+				res.status(200).send(req.params.Username + ' was deleted');
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
 });
 
 //	Allows users to add movie to favourites
-app.post('/users/:user/favourites', (req, res) => {
-	res.send('This movie has been added to your favourites');
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+	Users.findOneAndUpdate(
+		{ Username: req.params.Username },
+		{ $push: { FavouriteMovies: req.params.MovieID } },
+		{ new: true }, // This line makes sure that the updated document is returned
+		(err, updatedUser) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send('Error: ' + err);
+			} else {
+				res.json(updatedUser);
+			}
+		}
+	);
 });
 
 //	Allows users to remove movie to favourites
-app.delete('/users/:user/favourites', (req, res) => {
-	res.send('This movie has been removed to your favourites');
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+	Users.findOneAndUpdate(
+		{ Username: req.params.Username },
+		{ $pull: { FavouriteMovies: req.params.MovieID } },
+		{ new: true }, // this line makes sure that the updated document is returned
+		(err, updatedUser) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send('Error: ' + err);
+			} else {
+				res.json(updatedUser);
+			}
+		}
+	);
 });
 
 // 	Error handling
@@ -213,6 +198,7 @@ app.use((err, req, res, next) => {
 	res.status(500).send('Something broke!');
 });
 
+// 	Listens for requests
 app.listen(8080, () => {
 	console.log('Your app is listening on port 8080');
 });
